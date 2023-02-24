b6e40ef633c9aa78b7f044a66ab9d9492213ff30
Omer A <omerzahidali@gmail.com>
2023-02-24 14:45:23 +0300

    Fixed ordering and allowed creation of posts.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -22,11 +22,11 @@ class PostsController < ApplicationController
   # POST /posts or /posts.json
   def create
     @post = Post.new(post_params)
+    @post.author_id = current_user.id
 
     respond_to do |format|
       if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
+        redirect_to root_url
       else
         format.html { render :new, status: :unprocessable_entity }
         format.json { render json: @post.errors, status: :unprocessable_entity }
